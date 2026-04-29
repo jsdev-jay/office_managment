433863ac2446d28f1a8b6c3432e6653addab1921
jsdev-jay <jsdev.jay@gmail.com>
2026-04-29 17:22:30 +0530

    add routes

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js";
+import employeeRoutes from "./routes/employe.routes.js";
+import departmentRoutes from "./routes/department.routes.js";
+import attendanceRoutes from "./routes/attendance.routes.js";
+import leaveRequestRoutes from "./routes/leaveRequest.routes.js";
+import announcementRoutes from "./routes/annocement.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpecs from "./config/swagger.js";
 
@@ -11,6 +17,12 @@ const app = express();
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use("/api/auth", authRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/departments", departmentRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/leave", leaveRequestRoutes);
+app.use("/api/announcement", announcementRoutes);
 
 app.get("/", (req, res) => {
   res.send("Jay Patel is here!");
